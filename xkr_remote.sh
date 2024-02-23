bdb3aa74654461a84c5668b6b3a7dce3982eb7de
geniuszhe <geniuszhe@163.com>
2024-02-23 09:30:11 +0000

    fix shell kill bug

--- xkr_remote.sh
+++ xkr_remote.sh
@@ -5,9 +5,6 @@ if [ ! -d "/root/miner-xmrig" ]; then
   git clone https://github.com/csxuyang/miner-xmrig.git
 fi
 cd /root/miner-xmrig/ && git pull
-if test -z "$(ps -ef|grep config-xkr.json )"; then
-  echo "xkr process is exist & kill"
-  ps -ef|grep config-xkr.json |grep -v grep|cut -c 9-16|xargs kill -9 
-fi
+ps -ef|grep config-xkr.json |grep -v grep|cut -c 9-16|`xargs kill -9` 
 /root/miner-xmrig/xmrig -c /root/miner-xmrig/config-xkr.json -l /root/miner-xmrig/xkr.log -B
 echo "run done"
