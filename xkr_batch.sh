c92da5f76c69807f2455ca96064b15010caac948
csxuyang <geniuszhe@163.com>
2024-02-26 07:28:57 +0000

    tst

--- xkr_batch.sh
+++ xkr_batch.sh
@@ -4,3 +4,4 @@ do
   echo "run "$ip
   ssh root@$ip "wget -O xkr_remote.sh https://github.com/csxuyang/miner-xmrig/raw/main/xkr_remote.sh && chmod +x xkr_remote.sh && ./xkr_remote.sh"
 done
+echo "all done"
