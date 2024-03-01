42a77b7833301fa77315abda6e35495a5de88ff0
geniuszhe <geniuszhe@163.com>
2024-03-01 23:55:17 +0800

    1

--- gaga.sh
+++ gaga.sh
@@ -5,5 +5,5 @@ sudo ./apphub service remove && sudo ./apphub service install
 sudo ./apphub service start
 ./apphub status
 sleep 5
-sudo ./apps/gaganode/gaganode config set --token=nhbbffnqwnazwuxb74c27ce484973bf2
-./apphub restart
+sudo ./apphub-linux-amd64/apps/gaganode/gaganode config set --token=nhbbffnqwnazwuxb74c27ce484973bf2
+./apphub-linux-amd64/apphub restart
