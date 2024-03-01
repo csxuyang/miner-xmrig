7b14bf2367baf7101db23e035edb2e1cfe8a53e5
geniuszhe <geniuszhe@163.com>
2024-03-01 18:39:16 +0800

    gaga

---
+++ gaga.sh
@@ -0,0 +1,8 @@
+#/bin/sbin
+curl -o apphub-linux-amd64.tar.gz https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz && tar -zxf apphub-linux-amd64.tar.gz && rm -f apphub-linux-amd64.tar.gz && cd ./apphub-linux-amd64
+sudo ./apphub service remove && sudo ./apphub service install
+sudo ./apphub service start
+./apphub status
+sleep 5
+sudo ./apps/gaganode/gaganode config set --token=nhbbffnqwnazwuxb74c27ce484973bf2
+./apphub restart
