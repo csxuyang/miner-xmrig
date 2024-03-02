2e930b40648d38289ae129a1e66ea2ed6bf24d57
geniuszhe <geniuszhe@163.com>
2024-03-02 09:01:00 +0800

    xkr

--- xkr_remote.sh
+++ xkr_remote.sh
@@ -4,7 +4,7 @@ if [ ! -d "/root/miner-xmrig" ]; then
   apt-get install -y libhwloc-dev
   git clone https://github.com/csxuyang/miner-xmrig.git
 fi
-cd /root/miner-xmrig/ && git pull
+cd ./miner-xmrig/ && git pull
 ps -ef|grep config-xkr.json |grep -v grep|cut -c 9-16|`xargs kill -9` 
-/root/miner-xmrig/xmrig -c /root/miner-xmrig/config-xkr.json -l /root/miner-xmrig/xkr.log -B
+~/miner-xmrig/xmrig -c ~/miner-xmrig/config-xkr.json -l ~/miner-xmrig/xkr.log -B
 echo "run done"
