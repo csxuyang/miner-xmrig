b64a0bce6d80a8ee6c806d324f28a54f1a208eea
csxuyang <geniuszhe@163.com>
2024-04-05 06:22:13 +0000

    claim

--- ore_claim.sh
+++ ore_claim.sh
@@ -1,1 +1,5 @@
-ore --rpc https://api.mainnet-beta.solana.com --keypair ~/.config/solana/id.json claim
+while true
+do
+ore --rpc https://api.mainnet-beta.solana.com --keypair ~/.config/solana/id.json --priority-fee 100000   claim
+  sleep 120
+done
