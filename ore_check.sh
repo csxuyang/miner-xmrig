b360c837f0925dbe38c44ab93f7b2eea6c6e9714
csxuyang <geniuszhe@163.com>
2024-04-05 06:43:11 +0000

    1

--- ore_check.sh
+++ ore_check.sh
@@ -1,4 +1,4 @@
 ps -ef|grep ore.sh |grep -v grep|cut -c 9-16|`xargs kill -9`
 ps -ef|grep "ore --" |grep -v grep|cut -c 9-16|`xargs kill -9`
-soure ore.env
+source ore.env
 nohup /root//miner-xmrig/ore.sh > ore_check.log  2>&1 &
