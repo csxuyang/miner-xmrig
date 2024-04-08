c3f6429f48f762a00d813a519de45a0d6fe65f57
csxuyang <geniuszhe@163.com>
2024-04-08 01:19:07 +0000

    claim

--- ore_claim.sh
+++ ore_claim.sh
@@ -1,5 +1,20 @@
 while true
 do
-ore --rpc https://api.mainnet-beta.solana.com --keypair ~/.config/solana/id.json --priority-fee 100000   claim
-  sleep 120
+
+	balance=`ore --rpc https://api.mainnet-beta.solana.com --keypair ~/.config/solana/id.json rewards`
+	limit="0.001"
+	echo $balance
+	IFS=''
+	array=($balance)
+	real=${array[0]}
+	echo $real
+
+	if [ `expr $real \> $limit` -eq 1 ] ; then
+	   echo 'balance is '$balance',claim'
+	   ore --rpc https://api.mainnet-beta.solana.com --keypair ~/.config/solana/id.json --priority-fee 100000   claim
+	else
+	   echo 'balance is '$balance',not claim'
+	fi
+
+	sleep 120
 done
