251efca20d01b374a1a63c3cecddb123357bc0e0
csxuyang <geniuszhe@163.com>
2024-04-09 06:39:06 +0000

    trans

--- ore_trans.sh
+++ ore_trans.sh
@@ -1,1 +1,9 @@
-spl-token transfer --owner /root/.config/solana/id.json --url https://wallet.okex.org/fullnode/sol/discover/rpc oreoN2tQbHXVaZsr3pf66A48miqcBXCDJozganhEJgz ALL 2ED9VuxQtRaEfDrDmJXf23Hy1KSho9oKPZjo3ci1GUW3 --fund-recipient
+
+b=`spl-token balance oreoN2tQbHXVaZsr3pf66A48miqcBXCDJozganhEJgz`
+limit="0.01"
+if [ `expr $b \> $limit` -eq 1 ] ; then
+  echo 'balance is '$b',trans'
+  spl-token transfer --owner /root/.config/solana/id.json --url https://wallet.okex.org/fullnode/sol/discover/rpc oreoN2tQbHXVaZsr3pf66A48miqcBXCDJozganhEJgz ALL 2ED9VuxQtRaEfDrDmJXf23Hy1KSho9oKPZjo3ci1GUW3 --fund-recipient
+else
+  echo 'balance is '$b',not trans'
+fi
