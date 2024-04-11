2a2b3005d424b3de6d73d2c0c2082749def457bd
csxuyang <geniuszhe@163.com>
2024-04-11 02:23:28 +0000

    batch

---
+++ ore_batch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+mine() {
+  local keyfile="$1"  
+  stillRunning=$(ps -ef |grep "$keyfile mine" |grep -v "grep")
+  if [ "$stillRunning" ] ; then
+	echo "ORE ${keyfile} service was already started by another way"
+  else
+	echo "Starting service ${keyfile} ..."
+	nohup ore \
+			--rpc  $rpc_addr\
+			--priority-fee 5000 \
+			--keypair $keyfile mine \
+			--threads 4 \
+			> ore${2}.log  2>&1 &
+
+  fi
+
+}
+
+source ore.env
+while true; do
+ 
+  for i in {0..0}; do
+    mine "/root/.config/solana/id${i}.json" "${i}"
+    sleep 120
+  done
+  
+done
