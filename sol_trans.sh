23259c3d24df4cd19c1e25a565c1f6404f5ab911
csxuyang <geniuszhe@163.com>
2024-04-12 07:47:33 +0000

    trans

--- sol_trans.sh
+++ sol_trans.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 priority_fee="500000"
-SENDER_ADDRESS="EU8rvwBvGZZxkGXmktb8tQK5gSLEXkhmWHrCSMv4iCdF"
+SENDER_ADDRESS=$1
 RPC_URL1="https://wallet.okex.org/fullnode/sol/discover/rpc"  # 替换为 Solana 区块链节点的 URL 地址
 REWARD_THRESHOLD=0.02
 
-i=$1;
+i=$2;
 while [ $i -lt 110 ];
 do
    echo $i;
